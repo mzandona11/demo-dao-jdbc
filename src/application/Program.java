e96f4b69f9e61e27562fb4291609701ff7421f4c
mzandona11 <143609052+mzandona11@users.noreply.github.com>
2026-08-05 11:56:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     static void main(String[] args) {
         Departament obj = new Departament(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Matheus", "matheuszandona11@gmail.com", new Date(), obj);
+
+        System.out.println(seller);
     }
 }
